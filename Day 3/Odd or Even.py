be34ba3e102795bb1cf217ca76335eaee8d5f2ec
ThePYGamer <armand.abasllari@gmail.com>
2022-09-28 16:26:43 +0200

    commit

--- Day 3/Odd or Even.py
+++ Day 3/Odd or Even.py
@@ -1,5 +1,5 @@
 print ("Check if a number is Even Or Odd")
-numbers = input("Write a number: ")
+numbers = int(input("Write a number: "))
 
 if numbers // 2:
     print ("The number you have entered is Even")

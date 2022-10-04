299bb7e8873dce0a488a706d661337698f636b02
ThePYGamer <armand.abasllari@gmail.com>
2022-10-04 13:20:41 +0200

    commit

---
+++ 100 Days of Code/Day 8 - Beginner - Function Parameters & Caesar Cipher/caesar-cipher/Caesar Cipher Part 2 - Decryption.py
@@ -0,0 +1,33 @@
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+text = input("Type your message:\n").lower()
+shift = int(input("Type the shift number:\n"))
+
+
+
+def encrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in plain_text:
+        position = alphabet.index(letter)
+        new_position = position + shift_amount
+        cipher_text += alphabet[new_position]
+    print(f"The encoded text is {cipher_text}")
+
+    
+
+def decrypt(plain_text, shift_amount):
+    cipher_text = ""
+    for letter in text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        cipher_text += alphabet[new_position]        
+    print(f"The decrypt text is {cipher_text}")
+
+
+
+
+if direction == "encode":
+    encrypt(plain_text=text, shift_amount=shift)
+elif direction == "decode":
+    decrypt(plain_text=text, shift_amount=shift)

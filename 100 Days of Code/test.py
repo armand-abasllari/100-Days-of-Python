bfaf5a80165565653a616972c268ac0e102aa178
ThePYGamer <armand.abasllari@gmail.com>
2022-10-16 11:48:59 +0200

    commit

--- 100 Days of Code/test.py
+++ 100 Days of Code/test.py
@@ -34,31 +34,28 @@ resources = {
     "coffee": 100,
 }
 
-user_choice =input("What would you like? (espresso/latte/cappuccino): ")
 
 
-def deplete_resources(menu_item):
-    """
-    If the user's choice is in the menu, then deplete the resources needed to make that choice.
+should_continue = True
 
-    :param menu_item: the name of the menu item the user chose
-    """
-    should_continue = True
+while should_continue:
+    user_choice =input("What would you like? (espresso/latte/cappuccino): ")
 
-    while should_continue:
 
+    def deplete_resources(menu_item):
+        """
+        If the user's choice is in the menu, then deplete the resources needed to make that choice.
+
+        :param menu_item: the name of the menu item the user chose
+        """
         if menu_item == "espresso" or menu_item == "latte" or menu_item == "cappuccino":
-            for resource in ("water", "milk", "coffee"):
-                needed = menu[menu_item]["ingredients"].get(resource, 0)
-                resources[resource] -= needed
+            for i in ("water", "milk", "coffee"):
+                needed = menu[menu_item]["ingredients"].get(i, 0)
+                resources[i] -= needed
         elif menu_item == "report":
             print(f"Water: {resources['water']}\
             \nMilk: {resources['milk']}\
             \nCoffee: {resources['coffee']}")
-        elif menu_item == "off":
-            should_continue = False
 
 
     deplete_resources(menu_item=user_choice)
-
-

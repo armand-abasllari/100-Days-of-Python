126e407b53772bac64c360cae26b1f5871cdebe4
armand-abasllari <armand.abasllari@gmail.com>
2022-10-21 16:39:40 +0200

    commit

--- 100 Days of Code/Intermediate/Day 16 - Intermediate - Object Oriented Programming (OOP)/main.py
+++ 100 Days of Code/Intermediate/Day 16 - Intermediate - Object Oriented Programming (OOP)/main.py
@@ -18,5 +18,5 @@ while is_on:
         money_machine.report()
     else:
         drink = menu.find_drink(choice)       
-        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost):
+        if coffee_maker.is_resource_sufficient(drink) and money_machine.make_payment(drink.cost): #type: ignore
           coffee_maker.make_coffee(drink)
